9809b868a48e1d208705f25340e88611624bf2a9
Jeremiah Hetrick <129803748+JHetrick03@users.noreply.github.com>
2025-06-04 16:36:40 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Render!"}
+
+@app.get("/jobs")
+def get_jobs():
+    return [{"name": "Make Ready Tool NESC Demo", "status": "active"}]
